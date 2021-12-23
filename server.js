e9dea6831faf31aefe6cf2c881ecf3a6d74f11e1
sarahecarter <secarter213@gmail.com>
2021-12-22 23:08:10 -0500

    changing endDate back to inactive for now

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const SymptomSchema = new mongoose.Schema({
     startDate: String,
     severity: {type: Number, min: 1, max: 10},
     notes: String,
-    endDate: String
+    inactive: Boolean
 })
 
 const Symptom = mongoose.model("Symptom", SymptomSchema)
